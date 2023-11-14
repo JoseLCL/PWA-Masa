c1f4cfb5f1499c8e1353c3e0ee155cabd9b6414b
Jose L. Chavez. L <chewi4600@gmail.com>
2023-11-13 19:10:56 -0700

    Add files via upload

--- serv-worker.js
+++ serv-worker.js
@@ -3,8 +3,8 @@ self.addEventListener('install', (event) => {
       caches.open('conversor-cache').then((cache) => {
         return cache.addAll([
           './',
-          '/styles.css',
-          '/script.js',
+          '/estilos.css',
+          '/masa.js',
         ]);
       })
     );
