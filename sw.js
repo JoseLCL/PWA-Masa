d358368dae5c1dbd3df90c669c8a4d9cda71c550
Jose L. Chavez. L <chewi4600@gmail.com>
2023-11-13 19:32:43 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// sw.js
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('conversor-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/estilos.css',
+        '/masa.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
